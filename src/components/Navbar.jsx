756b7cd1958f7578f70e83d180bed15b9d261a5b
NikolaJelavic <jelavicnikola@gmail.com>
2023-11-20 22:30:14 +0100

    URL fixed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,14 @@ export default function Navbar() {
   return (
     <nav className=" p-6 ">
   <ul className="flex justify-around space-x-4 w-3/5 mx-auto">
-    {/* <li>
-      <Link to="/home" className="text-pink-200 font-bold hover:text-pink-300 text-lg">Home</Link>
-    </li> */}
     <li>
-      <Link to="/about" className="text-pink-200 font-bold hover:text-pink-300 text-lg">About</Link>
+      <Link to="/mark-cayanan/about" className="text-pink-200 font-bold hover:text-pink-300 text-lg">About</Link>
     </li>
     <li>
-      <Link to="/works" className="text-pink-200 font-bold hover:text-pink-300 text-lg">Works</Link>
+      <Link to="/mark-cayanan/works" className="text-pink-200 font-bold hover:text-pink-300 text-lg">Works</Link>
     </li>
     <li>
-      <Link to="/contact" className="text-pink-200 font-bold hover:text-pink-300 text-lg">Contact</Link>
+      <Link to="/mark-cayanan/contact" className="text-pink-200 font-bold hover:text-pink-300 text-lg">Contact</Link>
     </li>
   </ul>
 </nav>
