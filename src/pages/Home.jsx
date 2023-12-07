caef44337262d91ae5c8e032ec3007d3d591c04a
NikolaJelavic <jelavicnikola@gmail.com>
2023-12-07 22:22:54 +0100

    background fixed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
 
         <div className="bg-transparent">
-            <h1 className="text-center">Here goes an image and it will be challenging :)</h1>
+            {/* <h1 className="text-center">Here goes an image and it will be challenging :)</h1> */}
               <img src="https://res.cloudinary.com/dvwnqprsp/image/upload/v1701392777/Mark-Cayanan/Full-body_Milan_2_e6kfhl.jpg" alt="Mark Cayanan" 
               className=" h-screen w-screen object-cover " />
         </div>
