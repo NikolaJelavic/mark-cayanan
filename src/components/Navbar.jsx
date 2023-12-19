58e766cc2f67dc9e5b306f1188ec14b5a509eb5f
NikolaJelavic <jelavicnikola@gmail.com>
2023-12-19 11:53:51 +0100

    home page improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,25 @@
 import { Link } from "react-router-dom";
 
+const NavLink = ({ to, children }) => (
+  <li>
+    <Link
+      to={to}
+      className="text-pink-300 font-bold hover:text-pink-400 text-2xl bg-slate-100 px-2 rounded-xl"
+    >
+      {children}
+    </Link>
+  </li>
+);
+
 export default function Navbar() {
   return (
     <nav className=" p-6   bg-transparent">
-      <ul className="flex justify-around space-x-4 w-3/5 mx-auto">
-          <li>
-          <Link
-            to="/"
-            className="text-pink-300 font-bold hover:text-pink-400 text-2xl"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/about"
-            className="text-pink-300  font-bold hover:text-pink-400 text-2xl"
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/works"
-            className="text-pink-300 font-bold hover:text-pink-400 text-2xl"
-          >
-            Works
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/contact"
-            className="text-pink-300 font-bold hover:text-pink-400 text-2xl"
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <ul className="flex justify-around space-x-4 w-3/5 mx-auto relative z-10">
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/about">About</NavLink>
+    <NavLink to="/works">Works</NavLink>
+    <NavLink to="/contact">Contact</NavLink>
+  </ul>
     </nav>
   );
 }
