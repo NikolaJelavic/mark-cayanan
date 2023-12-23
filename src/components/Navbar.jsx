72a5aab423436a34481d3a387de3ebe8e81998f6
NikolaJelavic <jelavicnikola@gmail.com>
2023-12-23 05:40:58 +0100

    dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const NavLink = ({ to, children }) => (
+const NavLink = ({ to, children, onClick }) => (
   <li>
     <Link
       to={to}
-      className="text-pink-300 font-bold hover:text-pink-400 text-2xl bg-slate-100 px-2 rounded-xl"
+      className="text-pink-300 font-bold hover:text-pink-400 text-2xl bg-slate-100 px-2 rounded-xl my-3"
+      onClick={onClick}
     >
       {children}
     </Link>
@@ -12,14 +14,43 @@ const NavLink = ({ to, children }) => (
 );
 
 export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className=" p-6   bg-transparent">
-      <ul className="flex justify-around space-x-4 w-3/5 mx-auto relative z-10">
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/about">About</NavLink>
-    <NavLink to="/works">Works</NavLink>
-    <NavLink to="/contact">Contact</NavLink>
-  </ul>
+    <nav className=" p-6 bg-transparent">
+      <div className="flex justify-between mx-auto relative z-10">
+      <button
+          className="lg:hidden text-white text-3xl focus:outline-none bg-pink-400 p-4 justify-left"
+          onClick={toggleMenu}
+        >
+          &#9776;
+        </button>
+        <ul
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } lg:flex lg:justify-evenly  lg:space-x-4 lg:w-full w-36 absolute lg:relative bg-pink-400 lg:bg-transparent top-full lg:top-0 left-0 lg:left-auto  lg:mt-0 p-4 lg:p-0 `}
+        >
+          <NavLink to="/" onClick={toggleMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/about" onClick={toggleMenu}>
+            About
+          </NavLink>
+          <NavLink to="/works" onClick={toggleMenu}>
+            Works
+          </NavLink>
+          <NavLink to="/presence" onClick={toggleMenu}>
+            Presence
+          </NavLink>
+          <NavLink to="/contact" onClick={toggleMenu}>
+            Contact
+          </NavLink>
+        </ul>
+     </div>
     </nav>
   );
 }
